ae5cdf4e6545f80e40a37796885862f6f6ce70e3
trevo-community <147546698+trevo-community@users.noreply.github.com>
2025-01-13 17:59:50 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+
+// token de seu Cluster do mongoDb
+
+
+const token = 'mongodb+srv://anikit:EPt96b3yMx3wmEC@cluster0.ukzkyjq.mongodb.net/?retryWrites=true&w=majority'; 
+
+module.exports = {
+token
+}
